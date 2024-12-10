33650095e980e0b655ff2cb7deac924bbf03253f
Mason Moore <70770256+masmoore@users.noreply.github.com>
2024-12-09 16:41:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -50,30 +50,58 @@ def webhook():
             else:
                 response = f"Sorry, we don't have any {flooring_type} flooring available."
 
-        # Handle the 'installation_cost' intent
+                # Handle the 'installation_cost' intent
         elif intent_name == 'installation_cost':
             flooring_type = req['queryResult']['parameters'].get('flooring_type')
-            size = req['queryResult']['parameters'].get('number')  # Corrected parameter name to 'number'
+            product_name = req['queryResult']['parameters'].get('product_name')  # New parameter
+            size = req['queryResult']['parameters'].get('number')  # Floor size
 
-            if flooring_type and size:
-                # Filter flooring data for the specified type
-                product = flooring_data[flooring_data['Type'].str.lower() == flooring_type.lower()]
-        
+            if flooring_type and product_name and size:
+                # Filter flooring data for the specified type and product name
+                product = flooring_data[
+                    (flooring_data['Type'].str.lower() == flooring_type.lower()) &
+                    (flooring_data['Product Name'].str.lower() == product_name.lower())
+                ]
+                
                 if not product.empty:
                     product = product.iloc[0]  # Take the first match
                     price_per_sq_ft = product['Price per Sq Ft']
                     install_cost_per_sq_ft = product['Installation Cost per Sq Ft']
 
-                # Calculate costs
-                total_material_cost = price_per_sq_ft * size
-                total_install_cost = install_cost_per_sq_ft * size
-                if size < 1000:
-                    total_install_cost += 250  # Minimum fee for small areas
-            
-                total_cost = total_material_cost + total_install_cost
-                response = f"The total cost to install {size} sq ft of {flooring_type} flooring is ${total_cost:.2f}."
+                    # Calculate costs
+                    total_material_cost = price_per_sq_ft * size
+                    total_install_cost = install_cost_per_sq_ft * size
+                    if size < 1000:
+                        total_install_cost += 250  # Minimum fee for small areas
+                    
+                    total_cost = total_material_cost + total_install_cost
+                    response = (
+                        f"The total cost to install {size} sq ft of {product_name} ({flooring_type}) flooring is "
+                        f"${total_cost:.2f}."
+                    )
+                else:
+                    response = f"Sorry, we don't have information on the product {product_name} under {flooring_type} flooring."
             else:
-                response = f"Sorry, we don't have information on {flooring_type} flooring."
+                response = "Please specify the flooring type, product name, and area size for installation."
+
+            return jsonify({"fulfillmentText": response})
+
+        # Handle the 'list_product_names' intent
+        elif intent_name == 'list_product_names':
+            flooring_type = req['queryResult']['parameters'].get('flooring_type')
+            if flooring_type:
+                # Filter products based on the specified flooring type
+                products = flooring_data[flooring_data['Type'].str.lower() == flooring_type.lower()]
+                if not products.empty:
+                    product_names = products['Product Name'].unique()
+                    response = f"The available {flooring_type} products are: {', '.join(product_names)}."
+                else:
+                    response = f"Sorry, we don't have any products under the {flooring_type} type."
+            else:
+                response = "Please specify a flooring type to get the product names."
+            return jsonify({"fulfillmentText": response})
+
+
         else:
             response = "Please specify the type of flooring and the area size for installation."
 
